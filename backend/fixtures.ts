81238f09c7e0873a7d830584ff393bde0db5cc7a
koog7 <k.ognev709@mail.com>
2024-09-12 12:46:48 +0600

    Добавлены фикстуры для альбомов

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import Artist from "./models/Artists";
+import Album from "./models/Albums";
 
 const run = async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/spotifyAPI');
     const db = mongoose.connection;
     try {
         await db.dropCollection('artists')
+        await db.dropCollection('albums')
     }catch (e) {
         console.log(e)
     }
@@ -27,6 +29,49 @@ const run = async () => {
             info: 'Canadian singer, songwriter, and record producer',
         }
     ]);
+
+
+    await Album.create([
+        {
+            artistId: '66e1a8f62a4dfa83326da082',
+            dataRelease: 2020,
+            photo: 'AfterHours.jpg',
+            title: 'After Hours'
+        },
+        {
+            artistId: '66e1a8f62a4dfa83326da082',
+            dataRelease: 2016,
+            photo: 'StarBoy.jpeg',
+            title: 'Star Boy'
+        },
+        {
+            artistId: '66e1a8f62a4dfa83326da081',
+            dataRelease: 2021,
+            photo: 'HappierThanEver.jpeg',
+            title: 'Happier Than Ever'
+        },
+        {
+            artistId: '66e1a8f62a4dfa83326da081',
+            dataRelease: 2016,
+            photo: 'FallAsleep.png',
+            title: 'WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?'
+        },
+        {
+            artistId: '66e1a8f62a4dfa83326da080',
+            dataRelease: 2020,
+            photo: 'OliverAlbum.jpeg',
+            title: 'Ugly is Beautiful: Shorter, Tricker & Uglier (Deluxe)'
+        },
+        {
+            artistId: '66e1a8f62a4dfa83326da080',
+            dataRelease: 2022,
+            photo: 'CowboyTears.png',
+            title: 'Cowboy Tears'
+        },
+    ]);
+
+
+
 
     await db.close();
 }
