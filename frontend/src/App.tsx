f046a5bf3ffefb1bbf469908defa9ba4abf64daa
koog7 <k.ognev709@mail.com>
2024-09-13 13:09:58 +0600

    Добавлена страница Not Found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {NavLink, Route, Routes} from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import ListAlbums from "./containers/ListAlbums.tsx";
 import TrackList from "./containers/TrackList.tsx";
@@ -8,17 +8,30 @@ const App = () => {
 
     return(
         <>
-            <Routes>
-                <Route path="/" element={(
-                   <Home />
-                )}/>
-                <Route path="/album/:id" element={(
-                    <ListAlbums/>
-                )}/>
-                <Route path="/tracks/:id" element={(
-                    <TrackList/>
-                )}/>
-            </Routes>
+            <div>
+                <div style={{backgroundColor: '#404040', width: '1000px', minHeight: '50px', padding: '2px'}}>
+                    <h2 style={{marginLeft: '10px'}}><NavLink className="nav-link" to="/"
+                                                              style={{textDecoration: 'none', color: 'white'}}>Lite
+                        Spotify</NavLink></h2>
+                </div>
+                <hr/>
+            </div>
+
+            <div style={{marginLeft:'170px'}}>
+                <Routes>
+                    <Route path="/" element={(
+                        <Home/>
+                    )}/>
+                    <Route path="/album/:id" element={(
+                        <ListAlbums/>
+                    )}/>
+                    <Route path="/tracks/:id" element={(
+                        <TrackList/>
+                    )}/>
+                </Routes>
+            </div>
+
+
         </>
     )
 };
