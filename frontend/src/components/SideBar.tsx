5847e188c97c893b953d2516b92979f4eec71ab4
koog7 <k.ognev709@mail.com>
2024-09-26 12:30:27 +0600

    Добавлено отображение роли пользователя

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -29,12 +29,13 @@ const SideBar = () => {
                         {userData && userData.username ? (
                             <div style={{minHeight:'500px'}}>
                                 <p className="welcome-message">Здравствуйте, {userData.username}!</p>
+                                {userData.role && (<p style={{backgroundColor: userData.role === 'admin' ? '#ff4d4d' : userData.role === 'user' ? '#4caf50' : '#4dff4d', padding: '5px',borderRadius: '5px',}}>Роль - <span style={{fontSize:'20px'}}>{userData.role}</span></p>)}
                                 <div style={{marginTop: '50px', display:'flex', flexDirection:'column'}}>
                                     <NavLink className={'trackH-btn'} to={'/track_history'}>Создать артиста</NavLink>
                                     <NavLink className={'trackH-btn'} to={'/track_history'}>Создать альбом</NavLink>
                                     <NavLink className={'trackH-btn'} to={'/track_history'}>Создать трек</NavLink>
                                     <NavLink className={'trackH-btn'} to={'/track_history'}>История прослушивания</NavLink>
-                                    <a style={{marginTop:'300px'}} onClick={logOut} className={'logout'}>Выйти из аккаунта</a>
+                                    <a style={{marginTop:'250px'}} onClick={logOut} className={'logout'}>Выйти из аккаунта</a>
                                 </div>
                             </div>
                         ) : (
