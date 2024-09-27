324704be6fb81f5971fcc03232405466334b9c68
koog7 <k.ognev709@mail.com>
2024-09-27 13:07:35 +0600

    Теперь токен передается автоматически если пользователь залогинен

--- frontend/src/axios/AxiosAPI.ts
+++ frontend/src/axios/AxiosAPI.ts
@@ -1,7 +1,24 @@
-import axios from 'axios';
+import axios, {AxiosHeaders, AxiosRequestConfig} from 'axios';
+import {RootState} from "../app/store.ts";
+import {Store} from "@reduxjs/toolkit";
 
 const axiosApi = axios.create({
     baseURL: 'http://localhost:8000'
 });
+
+
+export const addInterceptors = (store: Store<RootState>) => {
+    axiosApi.interceptors.request.use((config) => {
+        const token = store.getState().User.user?.token;
+
+        const headers = config.headers as AxiosHeaders;
+
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+
+        return config;
+    });
+};
 
 export default axiosApi;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,9 @@ import {BrowserRouter} from "react-router-dom";
 import {persistor, store} from "./app/store.ts";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
+import {addInterceptors} from "./axios/AxiosAPI.ts";
 
+addInterceptors(store);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
